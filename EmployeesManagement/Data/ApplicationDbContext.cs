f214dd0e0a7228f8cf6e6816e8511d5070b40497
Mutayyab Imran <mutayyab786yo@gmail.com>
2024-04-27 00:08:34 +0500

    Add project files.

---
+++ EmployeesManagement/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using EmployeesManagement.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeesManagement.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Designation> Designations { get; set; }
+        public DbSet<Bank> Banks { get; set; }
+        public DbSet<SystemCode> SystemCodes { get; set; }
+        public DbSet<SystemCodeDetail> SystemCodeDetails { get; set; }
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<City> Cities { get; set; }
+
+
+    }
+}
---
+++ EmployeesManagement/Models/LeaveType.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeesManagement.Models
+{
+    public class LeaveType:UserActivity
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+
+    }
+}
